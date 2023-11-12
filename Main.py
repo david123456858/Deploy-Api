cb8212ddf507f86cbcef9d69eb3d0656a54bb59b
david123456858 <juandavidperaltafuentes@gmail.com>
2023-11-11 21:34:14 -0500

    first commit

---
+++ Main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from src.controller.Dolar import Predicciones, Clear, GetDatos
+from src.controller.Dolar import Predicciones, Clear, GetDatos
+from src.controller.predicciones_diarias import get_data_prediccion
+import threading
+app = FastAPI()
+
+data_thread = threading.Thread(target=get_data_prediccion)
+data_thread.daemon = True
+data_thread.start()
+
+@app.get("/predicciones")
+def read_root():
+    data = GetDatos()
+    df = Clear(data)
+    return Predicciones(df)
---
+++ src/controller/predicciones_diarias.py
@@ -0,0 +1,17 @@
+
+import time
+from src.controller.Dolar import GetDatos,Clear,Predicciones
+def get_data_prediccion():
+    while True:
+        data = GetDatos()
+        
+        # Limpiar los datos
+        df = Clear(data)
+        
+        # Realizar predicciones
+        data = Predicciones(df)
+        
+        # Guardar o procesar las predicciones según sea necesario
+        
+        # Esperar un día antes de la próxima ejecución
+        time.sleep(24 * 60 * 60)  # Espera 24 horas
