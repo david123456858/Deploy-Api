9bd0af67da81d9d016246525758674fab9b7e338
david123456858 <juandavidperaltafuentes@gmail.com>
2023-11-13 16:30:40 -0500

    cors

--- Main.py
+++ Main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from src.controller.Dolar import Predicciones, Clear, GetDatos
 from src.controller.Dolar import Predicciones, Clear, GetDatos
 from src.controller.predicciones_diarias import get_data_prediccion
+from fastapi.middleware.cors import CORSMiddleware
 import threading
 app = FastAPI()
 
@@ -9,6 +10,14 @@ data_thread = threading.Thread(target=get_data_prediccion)
 data_thread.daemon = True
 data_thread.start()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+)
+    
 @app.get("/predicciones")
 def read_root():
     data = GetDatos()
